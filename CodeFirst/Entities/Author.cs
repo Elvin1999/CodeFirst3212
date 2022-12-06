9188cc5049a6d480ce4b245ecc1091b9dd936a80
Elvin Jamalzade <camalzadeelvin@itstep.edu.az>
2022-12-06 20:26:19 +0400

    Add project files.

---
+++ CodeFirst/Entities/Author.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst.Entities
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int? Age { get; set; }
+        public virtual  ICollection<Book> Books { get; set; }
+
+        public override string ToString()
+        {
+            return Firstname;
+        }
+    }
+}
---
+++ CodeFirst/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using CodeFirst.DataAccess;
+using CodeFirst.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CodeFirst
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        LibraryContext _context = new LibraryContext();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _context.Database.CreateIfNotExists();
+            #region Add Category
+
+
+            
+            Get();
+
+            #endregion
+
+        }
+
+        public async void Get()
+        {
+            if(_context.Categories.Count()==0)
+            await AddCategories();
+            var categories = _context.Categories;
+            categoriesGrid.ItemsSource = await categories.ToListAsync();
+
+            if (_context.Authors.Count() == 0)
+                await AddAuthors();
+            var authors = _context.Authors;
+            authorsGrid.ItemsSource=await authors.ToListAsync();
+        }
+
+        public async Task AddCategories()
+        {
+            _context.Categories.Add(new Category
+            {
+                Name = "Adventure"
+            });
+            _context.Categories.Add(new Category
+            {
+                Name = "Science-Fiction"
+            });
+            _context.Categories.Add(new Category
+            {
+                Name = "Programming"
+            });
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AddAuthors()
+        {
+            _context.Authors.Add(new Author
+            {
+                Firstname="Linus",
+                Lastname="Torvalds",
+                 Age=35
+            });
+            _context.Authors.Add(new Author
+            {
+                Firstname = "Leyla",
+                Lastname = "Mammadova",
+                Age = 20
+            });
+            _context.Authors.Add(new Author
+            {
+                Firstname = "Axmed",
+                Lastname = "Axmedli",
+                Age = 43
+            });
+            await _context.SaveChangesAsync();
+        }
+    }
+}
